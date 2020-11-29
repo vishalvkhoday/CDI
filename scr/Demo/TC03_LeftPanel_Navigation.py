4c81f189134175cb00dba472004537ecde4c4a2f
vishalvkhoday <vishalvkhoday@gmail.com>
2020-11-29 11:09:21 +0530

    Commit II on 29th Nov

--- scr/Demo/TC03_LeftPanel_Navigation.py
+++ scr/Demo/TC03_LeftPanel_Navigation.py
@@ -4,10 +4,6 @@ Created on Mar 15, 2020
 @author: DELL
 '''
 
-# from Lib.WebDriver import *
-# from Lib.Commonfunction import *
-# from Lib.ReportFunction import *
-
 from Commonfunction import *
 from ReportFunction import *
 from time import sleep
@@ -15,24 +11,27 @@ import random
 from datetime import date
 
 
-@allure.title("Validate Quote Creation")
+@allure.title("Validate all links in left panel")
 def test_CreateQuote ():
     try:
         LaunchBrowser()
-#         fn_NavigateTo_QuotesPage()
-        
-        fn_CaptureScreenShot("Pass", "Quote page navigation completed!!!")
-        fn_ClickAddProductBtn()
-        scrProdList =fn_SelectProduct()
-        fn_ClickAddCheckClose()
-        fn_verifyProductName(scrProdList)
-        fn_CaptureScreenShot("Pass", "Completed !!!")      
+        fn_CaptureScreenShot("Pass","After Browser Launch")
+        sleep(2)
+        fn_ClickHomeLink()
+#         fn_ClickRecentLink()
+        fn_ClickDashboardsLink()
+        fn_ClickActivitiesLink()
+        fn_ClickAccountsLink()
+        fn_ClickContactsLink()
+        fn_ClickOpportunitiesLink()
+        fn_ClickQuotesLink()
+        fn_CaptureScreenShot("Pass", "Completed !!!")
         fn_closeBrowser()
-        
         assert True
     except:
         fn_CaptureScreenShot("Fail", "Unknown error look for error in report")
         fn_closeBrowser()
+        assert False
         
         
-test_CreateQuote()
+# test_CreateQuote()
